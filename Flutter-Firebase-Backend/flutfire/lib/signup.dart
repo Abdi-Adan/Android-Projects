6326897445e46d8ac3a14c69c982e6efceffa5f7
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-04-08 21:23:58 +0300

    my changes

--- Flutter-Firebase-Backend/flutfire/lib/signup.dart
+++ Flutter-Firebase-Backend/flutfire/lib/signup.dart
@@ -8,72 +8,73 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-
-  String _email, _password;
+  String email;
+  String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(hintText: 'email'),
-                onChanged: (value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'password'),
-                onChanged: (value) {
-                  setState(() {
-                    _password = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RaisedButton(
-                child: Text("SignUp"),
-                color: Colors.deepOrange,
-                textColor: Colors.white,
-                elevation: 10,
-                onPressed: () {
-                  FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: _email, password: _password)
-                      .then((signedInUser) {
-                        UserManagement().storeNewUser(signedInUser, context);
-                      })
-                      .catchError((e) {
-                    print(e);
-                  });
-                },
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text("Already have an account?"),
-              SizedBox(
-                height: 10,
-              ),
-              RaisedButton(
-                child: Text("Login"),
-                color: Colors.deepOrange,
-                textColor: Colors.white,
-                elevation: 10,
-                onPressed: () {},
-              ),
-            ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(hintText: 'email'),
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  decoration: InputDecoration(hintText: 'password'),
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("SignUp"),
+                  color: Colors.deepOrange,
+                  textColor: Colors.white,
+                  elevation: 10,
+                  onPressed: () {
+                    FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                            email: email, password: password)
+                        .then((AuthResult user) {
+                      UserManagement().storeNewUser(user, context);
+                    }).catchError((e) {
+                      print(e);
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text("Already have an account?"),
+                SizedBox(
+                  height: 10,
+                ),
+                RaisedButton(
+                  child: Text("Login"),
+                  color: Colors.deepOrange,
+                  textColor: Colors.white,
+                  elevation: 10,
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
